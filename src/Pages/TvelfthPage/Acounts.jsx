874210e01854d79bc32007961da7f059ed4e6434
Max <max@192.168.0.102>
2026-08-10 15:36:44 +0400

    AcountRow

--- src/Pages/TvelfthPage/Acounts.jsx
+++ src/Pages/TvelfthPage/Acounts.jsx
@@ -1,13 +1,14 @@
 import AccountOpening from "./AccountOpening";
 import AccountBanner from "./AccountBanner";
 import AccountAccordion from "./AccountAccordion";
-
+import AccountDocuments from "./AccountDocuments";
 const Acount = () => {
   return (
     <div className="w-full  font-sans min-h-screen py-6">
    <AccountOpening />
    <AccountBanner />
    <AccountAccordion />
+   <AccountDocuments />
     </div>
   );
 };
