fc6a3e83dac389852135f2304b92d44f77b897e2
Max <max@192.168.0.102>
2026-08-10 15:38:56 +0400

    TvelftPageAnimation

--- src/Pages/TvelfthPage/Acounts.jsx
+++ src/Pages/TvelfthPage/Acounts.jsx
@@ -2,6 +2,7 @@ import AccountOpening from "./AccountOpening";
 import AccountBanner from "./AccountBanner";
 import AccountAccordion from "./AccountAccordion";
 import AccountDocuments from "./AccountDocuments";
+import AcountBankingBanner from "./AcountBankBanner";
 const Acount = () => {
   return (
     <div className="w-full  font-sans min-h-screen py-6">
@@ -9,6 +10,7 @@ const Acount = () => {
    <AccountBanner />
    <AccountAccordion />
    <AccountDocuments />
+   <AcountBankingBanner />
     </div>
   );
 };
