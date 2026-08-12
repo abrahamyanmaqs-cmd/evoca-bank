e87db2604cd11f3783f9acc716dca058c5ef684d
Max <max@192.168.0.102>
2026-08-13 00:29:05 +0400

    EvocasNews

--- src/Pages/FiftheenthPage/EvocaSALARY.jsx
+++ src/Pages/FiftheenthPage/EvocaSALARY.jsx
@@ -1,10 +1,14 @@
 import EvocaSalaryHero from "./EvocaSalaryHero";
 import EvocaSalaryContent from "./EvocaSalaryContent";
+import EvocasNews from "./EvocasNewss";
+
+
 const EvocaSALARY = () => {
   return (
     <div className="w-full bg-[#F8F9FA] font-sans min-h-screen py-6">
     <EvocaSalaryHero />
     <EvocaSalaryContent />
+    <EvocasNews />
     </div>
   );
 };
