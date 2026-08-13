845bff54a54c2752a92b70917010be8348bd544e
Max <max@192.168.0.102>
2026-08-14 00:56:25 +0400

    EvocasTouchText

--- src/Pages/SixtheenthPage/EvocaTouch.jsx
+++ src/Pages/SixtheenthPage/EvocaTouch.jsx
@@ -1,8 +1,8 @@
-
+import EvocaTouchHero from "./EvocaTouchHero";
    const EvocaTouch = () => {
   return (
     <div className="w-full font-sans min-h-screen py-6">
-       
+       <EvocaTouchHero />
     </div>
   );
 };
