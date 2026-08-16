eb4e579070ea59ec4a05ae08df860d84a21455bf
Max <max@192.168.0.102>
2026-08-17 00:28:15 +0400

    end

--- src/Pages2/TwentyTwothPage/tarifs.jsx
+++ src/Pages2/TwentyTwothPage/tarifs.jsx
@@ -1,11 +1,38 @@
-import TariffTitle from "./TariffTitle";
-import TariffSidebar from "./TariffSidebar";
-const Tariff = () => {
+ import React from 'react';
+import TariffSidebar from './TariffSidebar'; // Ձեր ստեղծած մենյուի ֆայլը
+import EvocaHomeTable from './EvocaHomeTable'; // Ձեր ստեղծած աղյուսակի ֆայլը
+
+export default function Tariff() {
   return (
-    <div className="w-full font-sans min-h-screen py-6">
-   <TariffTitle />
-<TariffSidebar />
-    </div>
+    <section className="w-full py-10 px-4 md:px-16 bg-white font-sans select-none">
+      <div className="max-w-[1440px] mx-auto">
+        
+        {/* Գլխավոր վերնագիր (ամբողջ լայնքով) */}
+        <h1 className="text-2xl md:text-[32px] leading-tight font-extrabold text-[#1C1C1E] mb-10">
+          Ֆիզիկական անձանց տրամադրվող անհատական վարկեր ըստ պրոդուկտների / Պայմաններ և սակագներ /
+        </h1>
+
+        {/* Կողային մենյու և աղյուսակ (երկու սյունակով Flexbox դասավորություն) */}
+        <div className="flex flex-col lg:flex-row gap-8 lg:gap-12">
+          
+          {/* Ձախ կողմ՝ Մենյու */}
+          <div className="w-full lg:w-[320px] flex-shrink-0">
+            <TariffSidebar />
+          </div>
+
+          {/* Աջ կողմ՝ Ենթավերնագիր և Աղյուսակ */}
+          <div className="w-full lg:w-[calc(100%-368px)]">
+            {/* Ենթավերնագիր՝ աղյուսակի վերևում */}
+            <h2 className="text-xl md:text-2xl font-bold text-[#1C1C1E] mb-6">
+              Վերանորոգման վարկ EvocaHOME
+            </h2>
+            
+            {/* Աղյուսակ */}
+            <EvocaHomeTable />
+          </div>
+
+        </div>
+      </div>
+    </section>
   );
-};
- export default Tariff;
+}
