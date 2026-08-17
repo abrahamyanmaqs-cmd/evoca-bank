48327d875afa76ae1150893254ff35bd078a1638
Max <max@Maxs-MacBook-Air.local>
2026-08-17 14:42:17 +0400

    AmlPageText

--- src/Pages2/ThrdinOnethPage/Aml.jsx
+++ src/Pages2/ThrdinOnethPage/Aml.jsx
@@ -1,8 +1,10 @@
    import AmlDiv from "./AmlDiv";
+   import AmlText from "./AmlText";
    const Aml = () => {
   return (
     <div className="w-full font-sans min-h-screen py-6">
         <AmlDiv  />
+        <AmlText />
  
     </div>
   );
