ce42e451538c39b1ed9533dddb016ac2565239fc
Max <max@192.168.0.102>
2026-08-17 19:25:11 +0400

    document

--- src/Pages2/ThrdinFourthPage/Vaults.jsx
+++ src/Pages2/ThrdinFourthPage/Vaults.jsx
@@ -1,12 +1,14 @@
  import VaultsBox from "./VaultsBox";
  import VaultsTable1 from "./vaultsTable1";
   import VaultsTable2 from "./vaultsTable2";
+  import VaultsDocument from "./vaultsDocument";
  const Vaults = () => {
   return (
     <div className="w-full font-sans min-h-screen py-6">
       <VaultsBox />
       <VaultsTable1 />
       <VaultsTable2 />
+      <VaultsDocument />
     </div>
   );
 };
