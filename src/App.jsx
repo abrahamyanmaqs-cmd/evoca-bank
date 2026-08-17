a20c5f7f9b09a92901c02cf0ae296874079ca97d
Max <max@192.168.0.102>
2026-08-17 20:51:38 +0400

    35Page

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ import Privacy from "./Pages2/ThirdinthPage/Privacy";
 import CardsRules from "./Pages2/ThirdinTwothPage/CardsRules";
 import Online from "./Pages2/ThrdinThreethPage/Online";
 import Vaults from "./Pages2/ThrdinFourthPage/Vaults";
+import Faq from "./Pages2/ThrdinFivePage/Faq";
 //2
 import News from "./Pages/SixthPage/News";  
 import Blog from "./Pages/SeventPage/Blog";
@@ -63,6 +64,7 @@ function App() {
           <Route path="/cards-rules" element={<CardsRules />} />
           <Route path="/online" element={<Online />} />
           <Route path="/vaults" element={<Vaults />} />
+           <Route path="/faq" element={<Faq />} />
           <Route path="news" element={<News />} />
           <Route path="blog" element={<Blog />} /> 
           <Route path="career" element={<Career />} />
---
+++ src/Pages2/ThrdinFivePage/Faq.jsx
@@ -0,0 +1,8 @@
+const Faq = () => {
+  return (
+    <div className="w-full font-sans min-h-screen py-6">
+      
+    </div>
+  );
+};
+ export default Faq;
