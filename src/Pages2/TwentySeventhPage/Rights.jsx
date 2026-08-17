f38187ba33cdfb7f4ce9671b7ba1ffadfe0b8ae2
Max <max@192.168.0.102>
2026-08-17 04:10:20 +0400

    27Pagetext

--- src/Pages2/TwentySeventhPage/Rights.jsx
+++ src/Pages2/TwentySeventhPage/Rights.jsx
@@ -1,7 +1,9 @@
+ import RightsContent from "./RightsContent";
+ 
  const Rights = () => {
   return (
     <div className="w-full font-sans min-h-screen py-6">
- 
+        <RightsContent />
     </div>
   );
 };
