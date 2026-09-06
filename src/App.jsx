7055dde2265bf3370e4da1460271ed9d3b9c2229
Max <max@192.168.0.121>
2026-09-07 01:03:14 +0400

    97page

--- src/App.jsx
+++ src/App.jsx
@@ -87,6 +87,7 @@ import TeamInternetPayment from "./Pages/93page/TeamInternetPayment";
 import VivaCellInternetPayment from "./Pages/94page/VivaCellInternetPayment";
 import RostelecomPayment from "./Pages/95page/RostelecomPayment";
 import InteractiveTvPayment from "./Pages/96page/InteractiveTvPayment";
+import CtvPayment from "./Pages/97page/CtvPayment";
 import Layout from "./Components/Layout"; 
  
 function App() {
@@ -134,8 +135,7 @@ function App() {
                 <Route path="/payments/internet-tv/vivacell-internet" element={<   VivaCellInternetPayment/>} />
                 <Route path="/payments/internet-tv/rostelecom" element={<RostelecomPayment/>} />
                 <Route path="/payments/internet-tv/interactive-tv" element={<InteractiveTvPayment/>} />
-                
-
+                <Route path="/payments/internet-tv/ctv" element={<  CtvPayment/>} />
           <Route path="about" element={<AboutUs />} />
           <Route path="payments/3" element={<UtilitySubCategories />} />
           <Route path="management" element={<Managment />} />
