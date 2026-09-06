d98524ef668896756d0cb952c1da22ffae0fdcf4
Max <max@192.168.0.121>
2026-09-07 01:08:38 +0400

    99page

--- src/App.jsx
+++ src/App.jsx
@@ -89,6 +89,7 @@ import RostelecomPayment from "./Pages/95page/RostelecomPayment";
 import InteractiveTvPayment from "./Pages/96page/InteractiveTvPayment";
 import CtvPayment from "./Pages/97page/CtvPayment";
 import YournetPayment from "./Pages/98page/YournetPayment";
+import ArpinetPayment from "./Pages/99page/ArpinetPayment";
 import Layout from "./Components/Layout"; 
  
 function App() {
@@ -138,6 +139,7 @@ function App() {
                 <Route path="/payments/internet-tv/interactive-tv" element={<InteractiveTvPayment/>} />
                 <Route path="/payments/internet-tv/ctv" element={<  CtvPayment/>} />
                   <Route path="/payments/internet-tv/yournet" element={< YournetPayment/>} />
+                  <Route path="/payments/internet-tv/arpinet" element={< ArpinetPayment/>} /> 
           <Route path="about" element={<AboutUs />} />
           <Route path="payments/3" element={<UtilitySubCategories />} />
           <Route path="management" element={<Managment />} />
