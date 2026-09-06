02b36565d5b9b6964e1898a67937fd9f6a4ead14
Max <max@192.168.0.121>
2026-09-06 13:41:06 +0400

    85page

--- src/App.jsx
+++ src/App.jsx
@@ -75,6 +75,7 @@ import BlessLoanPayment1 from "./Pages/81page/BlessLoanPayment1";
 import BlessLoanPayment2 from "./Pages/82page/BlessLoanPayment2";
 import BlessLoanPayment3 from "./Pages/83page/BlessLoanPayment3";
 import RoadPoliceCamerasPayment from "./Pages/84page/RoadPoliceCamerasPayment";
+import GoodlinePayment from "./Pages/85page/GoodlinePayment";
 import Layout from "./Components/Layout"; 
  
 function App() {
@@ -110,6 +111,7 @@ function App() {
                <Route path="/payments/loans/finca" element={<BlessLoanPayment2 />} />
                 <Route path="/payments/loans/micro-capital" element={<BlessLoanPayment3 />} />
                 <Route path="/payments/road-police/cameras" element={<RoadPoliceCamerasPayment />} />
+                <Route path="/payments/international/goodline" element={<GoodlinePayment />} />
           <Route path="about" element={<AboutUs />} />
           <Route path="payments/3" element={<UtilitySubCategories />} />
           <Route path="management" element={<Managment />} />
